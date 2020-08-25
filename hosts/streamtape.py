0e7190e9e034135c3e2311aa37b93278fbced5d0
An0nimia <41164945+An0nimia@users.noreply.github.com>
2020-08-25 13:51:10 +0200

    Added exception when video isn't found

--- hosts/streamtape.py
+++ hosts/streamtape.py
@@ -2,6 +2,7 @@
 
 from requests import get
 from bs4 import BeautifulSoup
+from hosts.exceptions.exceptions import VideoNotAvalaible
 
 class Metadata:
 	def __init__(self):
@@ -12,11 +13,14 @@ def get_video(url):
 	body = get(url).text
 	parsing = BeautifulSoup(body, "html.parser")
 
-	video_link = (
-		parsing
-		.find("div", id = "videolink")
-		.get_text()
-	)
+	try:
+		video_link = (
+			parsing
+			.find("div", id = "videolink")
+			.get_text()
+		)
+	except AttributeError:
+		raise VideoNotAvalaible(url)
 
 	video_url = "https:%s" % video_link
 	return video_url
